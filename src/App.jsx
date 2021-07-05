d885c8a8f8fa2826d6151bf8a699ac42c5ca0a54
Nathan Tang <nathan.ly.tang@gmail.com>
2021-07-05 03:06:04 -0600

    Test fetch requests login

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,29 @@ class App extends Component {
         this.state = { };
     }
 
+    async login() {
+        const res = await fetch('http://127.0.0.1:9000/users/login', {
+            method: 'POST',
+            mode: 'cors',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({email: "test5@email", password: "password"})
+        })
+        const data = await res.json();
+        console.log(res);
+        console.log(data.details.token);
+        // document.cookie = `token=${data.details.token};httpOnly:True`
+        // document.cookie(res.json().details);
+        
+    }
+
     render() {
         console.log("test");
         return (
             <div className="App">
-                <p>test</p>
+                <button onClick={this.login}>testButton</button>
             </div>
         );
     }
