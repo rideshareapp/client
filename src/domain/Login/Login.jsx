8fd496f8b9dc22a953749a55c9577d5969e4a35e
Nathan Tang <nathan.ly.tang@gmail.com>
2021-07-19 23:18:52 -0600

    Feat: custom CSS for login page

--- src/domain/Login/Login.jsx
+++ src/domain/Login/Login.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
-import loginbanner from "../../assets/images/loginbanner.jpg";
+import { Link, useHistory } from "react-router-dom";
+import loginbanner from "../../assets/images/loginbanner1.jpg";
 import { useAppContext } from "../../libs/contextLib";
 // import { useFormFields } from "../../libs/hooksLib";
+import "./Login.css";
 
 function Login() {
 
@@ -41,31 +42,63 @@ function Login() {
     }
 
     return (
-        <div className="login">
-            <div className="container">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-6">
-                            <img className="img-fluid rounded mb-4 mb-lg-0" src={loginbanner} alt="Login banner" />
-                        </div>
-                        <div className="col-6">
-                            <h1 className="font-weight-light">Login</h1>
-                            <form onSubmit={handleSubmit}>
+        // <div className="login">
+        //     <div className="container">
+        //         <div className="container">
+        //             <div className="row">
+        //                 <div className="col-6">
+        //                     <img className="img-fluid rounded mb-4 mb-lg-0" src={loginbanner} alt="Login banner" />
+        //                 </div>
+        //                 <div className="col-6">
+        //                     <h1 className="font-weight-light">Login</h1>
+        //                     <form onSubmit={handleSubmit}>
 
-                                <div className="form-group">
-                                    <label htmlFor="emailInput" >Email</label>
-                                    <input autoFocus type="email" className="form-control" id="emailInput" aria-describedby="emailHelp" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)}></input>
-                                </div>
+        //                         <div className="form-group">
+        //                             <label htmlFor="emailInput" >Email</label>
+        //                             <input autoFocus type="email" className="form-control" id="emailInput" aria-describedby="emailHelp" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)}></input>
+        //                         </div>
 
-                                <div className="form-group">
-                                    <label htmlFor="passwordInput">Password</label>
-                                    <input type="password" className="form-control" id="passwordInput" placeholder="Password" onChange={(e) => setPassword(e.target.value)}></input>
-                                </div>
-                                
-                                <button type="submit" className="btn btn-primary" disabled={!validateForm()}>Login</button>
-                            </form>
-                        </div>
+        //                         <div className="form-group">
+        //                             <label htmlFor="passwordInput">Password</label>
+        //                             <input type="password" className="form-control" id="passwordInput" placeholder="Password" onChange={(e) => setPassword(e.target.value)}></input>
+        //                         </div>
+
+        //                         <button type="submit" className="btn btn-primary" disabled={!validateForm()}>Login</button>
+        //                     </form>
+        //                     <Link to="/signup">New here? Signup</Link>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
+
+        <div className="wrapper">
+            {/* <div className="image">
+
+            </div> */}
+            <div className="image">
+                <img src={loginbanner} alt="Login banner" />
+            </div>
+            <div className="formSection">
+                <div className="logo">
+                    Rideshareapp
+                </div>
+                <form className="form" onSubmit={handleSubmit}>
+
+                    <div>
+                        <label htmlFor="emailInput" >Email Address</label>
+                        <input autoFocus type="email" id="emailInput" aria-describedby="emailHelp" onChange={(e) => setEmail(e.target.value)}></input>
                     </div>
+
+                    <div>
+                        <label htmlFor="passwordInput">Password</label>
+                        <input type="password" id="passwordInput" onChange={(e) => setPassword(e.target.value)}></input>
+                    </div>
+
+                    <button type="submit" disabled={!validateForm()}>Next</button>
+                </form>
+                <div style={{ textAlign: "center", fontSize: "0.9rem" }} >
+                    <p>Not a member? &#8211; <Link to="/signup">Sign up here</Link></p>
                 </div>
             </div>
         </div>
