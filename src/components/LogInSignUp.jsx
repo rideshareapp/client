5b5d186048c25e43e6407c9f8162f3e7d7bb1948
Nathan Tang <nathan.ly.tang@gmail.com>
2021-07-21 20:00:43 -0600

    Login and signup page uses same grid layout

---
+++ src/components/LogInSignUp.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+import { Link } from "react-router-dom";
+import loginbanner from "../assets/images/loginbanner1.jpg";
+// import { useFormFields } from "../../libs/hooksLib";
+import styles from "./LogInSignUp.module.css";
+
+function LogInSignUpGrid(props) {
+
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.image}>
+                <img src={loginbanner} alt="Banner" />
+            </div>
+            <div className={styles.formSection}>
+                <div className={styles.logo}>
+                    Rideshareapp
+                </div>
+                {props.form}
+                <div className={styles.prompt} >
+                    <p>{props.prompt1} &#8211; <Link style={{}} to={props.prompt2}>{props.prompt3}</Link></p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default LogInSignUpGrid;
