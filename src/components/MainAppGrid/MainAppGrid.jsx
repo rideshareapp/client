5ab8116a8bb9c5f6864a55d231e1814aade248ad
Nathan Tang <nathan.ly.tang@gmail.com>
2021-07-27 21:24:51 -0600

    Feat: Main grid controls sidebar size

--- src/components/MainAppGrid/MainAppGrid.jsx
+++ src/components/MainAppGrid/MainAppGrid.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./MainAppGrid.module.css";
 import { Navigation } from "../../components";
 
 function MainAppGrid(props) {
-
+    const [expand, setExpand] = useState(true);
+    // TODO: Fix CSS to display grid properly at 10% on medium
     return (
-        <div className={styles.wrapper}>
+        <div className={`${expand ? styles.wrapperExpanded : styles.wrapperCollapsed}`}>
             <div className={styles.navigation}>
-                <Navigation />
+                <Navigation expand={expand} setExpand={setExpand} />
             </div>
             <div className={styles.mainContent}>
                 {props.content}
