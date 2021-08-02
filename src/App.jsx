948410fa8c848220dc8e8eb5e3dd392e6855a818
Nathan Tang <nathan.ly.tang@gmail.com>
2021-08-01 23:31:28 -0600

    Redirects to events instead of dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
-import { Login, Logout, Signup, Dashboard, NotFound, Events, Requests } from "./domain";
+import { Login, Logout, Signup, NotFound, Events, Requests } from "./domain";
 import { AppContext } from "./libs/contextLib";
 import "./App.css";
 
@@ -48,12 +48,12 @@ function App() {
                     <Router>
                         <Switch>
                             <Route exact path="/">
-                                {isAuthenticated ? <Redirect to="/dashboard" /> : <Redirect to="/login" />}
+                                {isAuthenticated ? <Redirect to="/events" /> : <Redirect to="/login" />}
                             </Route>
                             <Route path="/login" exact component={() => isAuthenticated ? <Redirect to="/" /> : <Login />} />
                             <Route path="/signup" exact component={() => isAuthenticated ? <Redirect to="/" /> : <Signup />} />
 
-                            <Route path="/dashboard" exact component={() => isAuthenticated ? <Dashboard /> : <Redirect to="/" />} />
+                            {/* <Route path="/dashboard" exact component={() => isAuthenticated ? <Dashboard /> : <Redirect to="/" />} /> */}
                             <Route path="/events" exact component={() => isAuthenticated ? <Events /> : <Redirect to="/" />} />
                             <Route path="/requests" exact component={() => isAuthenticated ? <Requests /> : <Redirect to="/" />} />
                             <Route path="/logout" exact component={() => isAuthenticated ? <Logout /> : <Redirect to="/" />} />
