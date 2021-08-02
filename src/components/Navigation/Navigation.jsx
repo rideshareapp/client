0135415aefa7355826fe2cb8260cdc1204293575
Nathan Tang <nathan.ly.tang@gmail.com>
2021-08-01 23:32:40 -0600

    Feat: Arrow images

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,6 +2,8 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
 import styles from "./Navigation.module.css";
+import arrowRight from "../../assets/icons/arrow-bar-right.svg";
+import arrowLeft from "../../assets/icons/arrow-bar-left.svg";
 
 function Navigation(props) {
 
@@ -13,28 +15,7 @@ function Navigation(props) {
     function setNav() {
         return props.expand ? styles.navExpanded : styles.navCollapsed;
     }
-    // TODO: Let links take 100% of width
-    // TODO: Transition when switching sidebar sizes
     return (
-        // <div className="navigation">
-        //     <nav className="navbar navbar-expand navbar-dark bg-dark">
-        //         <div className="container">
-        //             <Link className="navbar-brand" to="/">Rideshareapp</Link>
-        //             <div>
-        //                 <ul className="navbar-nav ml-auto">
-        //                     <li className={`nav-item  ${props.location.pathname === "/page2" ? "active" : ""}`}>
-        //                         <Link className="nav-link" to="/page">Page2</Link>
-        //                     </li>
-        //                     <li className={`nav-item  ${props.location.pathname === "/logout" ? "active" : ""}`}>
-        //                         <Link className="nav-link" to="/logout">Logout</Link>
-        //                     </li>
-
-        //                 </ul>
-        //             </div>
-        //         </div>
-        //     </nav>
-        // </div>
-
         <div className={`${styles.sidebar} ${setNav()}`}>
             <div className={styles.logo}>
                 <h1>Rideshareapp</h1>
@@ -53,7 +34,7 @@ function Navigation(props) {
                 </ul>
             </nav>
             <div className={styles.collapseExpandButton}>
-                <span style={{cursor: "pointer"}} onClick={handleExpand}>{props.expand ? "←" : "→"}</span>
+                <img style={{ cursor: "pointer" }} onClick={handleExpand} src={props.expand ? arrowLeft : arrowRight} />
             </div>
         </div>
     );
