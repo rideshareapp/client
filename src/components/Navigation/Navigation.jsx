ca7d551183b7b6e3f6dc99220fcb2c5e7dac8c7b
Nathan Tang <nathan.ly.tang@gmail.com>
2021-08-02 23:33:38 -0600

    Feat: Set icon and text dependant on expansion

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,35 +1,54 @@
-/* eslint-disable react/prop-types */
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
 import styles from "./Navigation.module.css";
 import arrowRight from "../../assets/icons/arrow-bar-right.svg";
 import arrowLeft from "../../assets/icons/arrow-bar-left.svg";
+import eventsIcon from "../../assets/icons/calendar-check.svg";
+import eventsIconSel from "../../assets/icons/calendar-check-fill.svg";
+import requestsIcon from "../../assets/icons/chat-square-quote.svg";
+import requestsIconSel from "../../assets/icons/chat-square-quote-fill.svg";
+import logout from "../../assets/icons/box-arrow-right.svg";
+import settings from "../../assets/icons/gear.svg";
+import settingsSel from "../../assets/icons/gear-fill.svg";
 
 function Navigation(props) {
 
-
     function handleExpand() {
         props.setExpand(!props.expand);
+        window.localStorage.setItem('expand', !props.expand);
     }
 
     function setNav() {
         return props.expand ? styles.navExpanded : styles.navCollapsed;
     }
+
+    function setIcon(path, icon, iconSel, title) {
+        const src = props.location.pathname === `/${path}` ? iconSel : icon;
+        return <img src={src} alt={title} title={title} className={styles.icon} />;
+    }
+
+    function setActive(path) {
+        return props.location.pathname === `/${path}` ? styles.active : "";
+    }
+
     return (
         <div className={`${styles.sidebar} ${setNav()}`}>
             <div className={styles.logo}>
-                <h1>Rideshareapp</h1>
+                <h1>{props.expand ? "Rideshareapp" : "R"}</h1>
             </div>
             <nav className={styles.nav}>
                 <ul className={styles.list}>
-                    <li className={`${props.location.pathname === "/events" ? styles.active : ""} ${styles.listItem}`}>
-                        <Link to="/events">Events</Link>
+                    <li className={`${setActive("events")} ${styles.listItem}`}>
+                        <Link to="/events">{setIcon("events", eventsIcon, eventsIconSel, "Events")}{props.expand ? " Events" : ""}</Link>
                     </li>
-                    <li className={`${props.location.pathname === "/requests" ? styles.active : ""} ${styles.listItem}`}>
-                        <Link to="/requests">Requests</Link>
+                    <li className={`${setActive("requests")} ${styles.listItem}`}>
+                        <Link to="/requests">{setIcon("requests", requestsIcon, requestsIconSel, "Requests")}{props.expand ? " Requests" : ""}</Link>
                     </li>
-                    <li className={`${props.location.pathname === "/logout" ? styles.active : ""} ${styles.listItem}`}>
-                        <Link to="/logout">Logout</Link>
+                    <li className={`${setActive("settings")} ${styles.listItem}`}>
+                        <Link to="/settings">{setIcon("settings", settings, settingsSel, "Settings")}{props.expand ? " Settings" : ""}</Link>
+                    </li>
+                    <li className={`${setActive("logout")} ${styles.listItem}`}>
+                        <Link to="/logout">{setIcon("logout", logout, logout, "Logout")}{props.expand ? " Logout" : ""}</Link>
                     </li>
                 </ul>
             </nav>
