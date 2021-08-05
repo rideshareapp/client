f026b348255e8eda500bd610e6f6a2114838e3d9
Nathan Tang <nathan.ly.tang@gmail.com>
2021-08-05 03:20:40 -0600

    Check cookies for valid refresh token before calling for refresh token

--- src/libs/authLib.js
+++ src/libs/authLib.js
@@ -1,6 +1,5 @@
 export default async function HandleAuth(userIsAuthenticated) {
     try {
-        
         const res = await fetch(`http://127.0.0.1:9000/auth/validToken`, {
             method: 'GET',
             mode: 'cors',
@@ -9,6 +8,12 @@ export default async function HandleAuth(userIsAuthenticated) {
         if (res.ok) {
             userIsAuthenticated(true);
         } else if (res.status === 401) {
+            let cookies = getCookies();
+            if (cookies.REFRESH_TOKEN_VALID !== "true") {
+                userIsAuthenticated(false);
+                return;
+            }
+
             const res = await fetch(`http://127.0.0.1:9000/auth/refresh`, {
                 method: 'POST',
                 mode: 'cors',
@@ -24,4 +29,13 @@ export default async function HandleAuth(userIsAuthenticated) {
     catch (err) {
         alert(err);
     }
+}
+
+function getCookies() {
+    let cookies = document.cookie.split('; ').reduce((prev, current) => {
+        const [name, ...value] = current.split('=');
+        prev[name] = value.join('=');
+        return prev;
+    }, {});
+    return cookies;
 }
