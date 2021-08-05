19e348d033cbe428dbf51c1f1abdd0913531b460
Nathan Tang <nathan.ly.tang@gmail.com>
2021-08-04 20:48:44 -0600

    Ref: Delete misc

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 export { default as Navigation } from "./Navigation/Navigation";
-export { default as Footer } from "./Footer";
 export { default as LogInSignUpGrid } from "./LogInSignUpGrid/LogInSignUp";
 export { default as MainAppGrid } from "./MainAppGrid/MainAppGrid";
 export { default as EventCard } from "./EventCard/EventCard";
