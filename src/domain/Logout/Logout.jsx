422b2d05b34989171dcadb9796c58b8b35238baf
Nathan Tang <nathan.ly.tang@gmail.com>
2021-08-04 20:47:54 -0600

    Fix: useEffect hook in top level of function body

--- src/domain/Logout/Logout.jsx
+++ src/domain/Logout/Logout.jsx
@@ -1,26 +1,22 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { useAppContext } from "../../libs/contextLib";
 
-function Logout() {
+export default function Logout() {
     const history = useHistory();
     const { userIsAuthenticated } = useAppContext();
 
-    function handleLogout() {
-        useEffect(() => {
-            fetch(`http://127.0.0.1:9000/users/logout`, {
-                method: 'POST',
-                mode: 'cors',
-                credentials: 'include',
-            });
-            userIsAuthenticated(false);
-            history.push("/login");
-        }, []);
-    }
+    useEffect(() => {
+        fetch(`http://127.0.0.1:9000/users/logout`, {
+            method: 'POST',
+            mode: 'cors',
+            credentials: 'include',
+        });
+        userIsAuthenticated(false);
+        history.push("/login");
+    }, [userIsAuthenticated, history]);
 
     return (
-        <>{handleLogout()}</>
+        null
     );
 }
-
-export default Logout;
