2934119237c828bb503a596161ab280a90022b85
Nathan Tang <nathan.ly.tang@gmail.com>
2021-08-07 20:26:05 -0600

    Feat: Display all information

--- src/components/EventCard/EventCard.jsx
+++ src/components/EventCard/EventCard.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./EventCard.module.css";
 
 export default function EventCard(props) {
+    const [date, setDate] = useState();
+    const [time, setTime] = useState();
+    // TODO: Allow wysiwyg functionality for description
+
+    useEffect(() => {
+        // let daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        let dateTimeString = new Date(props.date);
+        
+        setDate(`${dateTimeString.toDateString()}`);
+        setTime(dateTimeString.toLocaleTimeString());
+    }, [props.date]);
 
     return (
         <div className={styles.wrapper}>
-            <p>{props.event}</p>
-            <p>{props.date}</p>
+            <h1>{props.event}</h1>
+            <h2>{props.name}</h2>
+            <p>{props.description}</p>
+            <p>{props.location}</p>
+            <p>{date}</p>
+            <p>{props.include_time ? time : ""}</p>
         </div>
     );
 }
