cd5b895e9fd32f95eedafc619b70817b49130114
Nathan <nathan.ly.tang@gmail.com>
2021-08-20 01:46:16 -0700

    Feat: Display all event information in a card

--- src/components/EventCard/EventCard.jsx
+++ src/components/EventCard/EventCard.jsx
@@ -3,29 +3,37 @@ import styles from "./EventCard.module.css";
 import geo from "../../assets/icons/geo-alt.svg";
 
 export default function EventCard(props) {
-    const [date, setDate] = useState();
+    const [month, setMonth] = useState();
+    const [day, setDay] = useState();
     const [time, setTime] = useState();
     // TODO: Allow wysiwyg functionality for description
 
     useEffect(() => {
         // let daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
         let dateTimeString = new Date(props.date);
-
-        setDate(`${dateTimeString.toDateString()}`);
-        setTime(dateTimeString.toLocaleTimeString());
+        setMonth(`${dateTimeString.toLocaleDateString('default', { month: 'short' }).toLocaleUpperCase()}`);
+        setDay(`${dateTimeString.toLocaleDateString('default', { day: 'numeric' })}`);
+        setTime(dateTimeString.toLocaleTimeString('default', { hour: 'numeric', minute: '2-digit' }));
     }, [props.date]);
 
     return (
         <div className={styles.wrapper}>
             <div className={styles.dateTime}>
-                <div className={styles.date}>{date}</div>
-                <p>{props.include_time ? time : null}</p>
+                <div className={styles.date}>
+                    <div className={styles.month}>{month}</div>
+                    <div className={styles.day}>{day}</div>
+                </div>
+                <div className={styles.time}>
+                    {props.include_time ? <time>{time}</time> : null}
+                </div>
             </div>
             <div className={styles.info}>
-                <h1>{props.event}</h1>
-                <h2>{props.name}</h2>
-                <p>{props.description}</p>
-                <p><img src={geo} alt="Location" /> {props.location}</p>
+                <div className={styles.glance}>
+                    <h1>{props.event}</h1>
+                    <h2>{props.name}</h2>
+                    <h3><img src={geo} alt="Location" /> {props.location}</h3>
+                </div>
+                <p className={styles.description}>{props.description}</p>
             </div>
         </div>
     );
