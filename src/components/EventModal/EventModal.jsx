49f879011f7b3f622728a498e01cebc6f1c46ce5
Nathan <nathan.ly.tang@gmail.com>
2021-08-29 12:09:05 -0600

    Feat: event modal box

--- src/components/EventModal/EventModal.jsx
+++ src/components/EventModal/EventModal.jsx
@@ -6,7 +6,7 @@ export default function EventModal(props) {
     const [month, setMonth] = useState();
     const [day, setDay] = useState();
     const [time, setTime] = useState();
-    // TODO: Allow wysiwyg functionality for description
+
 
     useEffect(() => {
         // let daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
@@ -16,28 +16,33 @@ export default function EventModal(props) {
         setTime(dateTimeString.toLocaleTimeString('default', { hour: 'numeric', minute: '2-digit' }));
     }, [props.date]);
 
-    // function handleClick() {
-    //     console.log('clicked');
+    // function handleKeyDown(event) {
+    //     if (event.keyCode === 27) {
+    //         console.log("keypress");
+    //     }
     // }
 
     return (
-        <div className={styles.wrapper} onClick={props.onClick}>
-            <div className={styles.dateTime}>
-                <div className={styles.date}>
-                    <div className={styles.month}>{month}</div>
-                    <div className={styles.day}>{day}</div>
+        <div tabIndex="0" className={styles.box} onClick={props.onClick}>
+            <div className={styles.wrapper}>
+                <div className={styles.dateTime}>
+                    <div className={styles.date}>
+                        <div className={styles.month}>{month}</div>
+                        <div className={styles.day}>{day}</div>
+                    </div>
+                    <div className={styles.time}>
+                        {props.include_time ? <time>{time}</time> : null}
+                    </div>
                 </div>
-                <div className={styles.time}>
-                    {props.include_time ? <time>{time}</time> : null}
+                <div className={styles.info}>
+                    <div className={styles.glance}>
+                        <h1>{props.event}</h1>
+                        <h2>{props.name}</h2>
+                        <h3><img src={geo} alt="Location" /> {props.location}</h3>
+                    </div>
+                    <p className={styles.description}>{props.description}</p>
                 </div>
-            </div>
-            <div className={styles.info}>
-                <div className={styles.glance}>
-                    <h1>{props.event}</h1>
-                    <h2>{props.name}</h2>
-                    <h3><img src={geo} alt="Location" /> {props.location}</h3>
-                </div>
-                <p className={styles.description}>{props.description}</p>
+                <button onClick={props.onClick}>Exit</button>
             </div>
         </div>
     );
